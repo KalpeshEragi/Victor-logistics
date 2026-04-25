98e9385a5844c6edd2ddd73b23f621ae0007260e
KalpeshEragi <kalpesheragi@gmail.com>
2026-04-25 23:02:09 +0530

    chore: update branch contact information for Mumbai, Hyderabad, and Visakhapatnam

--- src/data/branches.ts
+++ src/data/branches.ts
@@ -4,15 +4,15 @@ export const branches = [
     lat: 19.076,
     lng: 72.8777,
     address: "Siddhivinayaka Complex, Shop no. 4 Plot no. 71, Sector 20, Kamothe, Navi Mumbai-410209 ",
-    phone: "022-65097666 / 8108967666",
-    email: "mumbai@victorexpressline.com"
+    phone: "+91 8108834666",
+    email: "cs.mum@victorexpressline.com"
   },
   {
     city: "Hyderabad",
     lat: 17.385,
     lng: 78.4867,
     address: "Building No. 276 & 287, Rama Krishna Nagar, Medipally, Hyderabad, Medchal Malkajgiri, Telangana – 500039",
-    phone: "040-27893670 / 9290760519",
+    phone: "+91 7799393666",
     email: "hyd@victorexpressline.com"
   },
   {
@@ -20,8 +20,8 @@ export const branches = [
     lat: 17.6868,
     lng: 83.2185,
     address: "VICTOREXPRESS LINE, G-3, Sarada Towers, Facor Layout, RamNagar, Visakhapatnam, andhrapradesh, 530002.",
-    phone: "0891-2576766",
-    email: "satish@victorexpress.com"
+    phone: "+91 7799393666",
+    email: "ho@victorexpressline.com"
   },
   {
     city: "Singapore",
